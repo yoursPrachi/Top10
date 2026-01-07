f73f3ed89aa182e713b42160d30800ed1826ee44
yoursPrachi <yogeshbalajiearth@gmail.com>
2026-01-07 16:02:10 +0530

    Delete app/page.tsx

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-// app/page.tsx
-import { redirect } from "next/navigation";
-
-export default function Home() {
-  // Automatically send user to hospital ranking page
-  redirect("/top-10-best-hospitals-in-new-york-2025");
-}
